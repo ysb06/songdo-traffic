a682c76bbfbb8ad28e7769123c470e7cb535ca9f
sbyim <ysb06@hotmail.com>
2024-11-05 12:13:00 +0900

    Update SARIMAX training

--- songdo_arima/src/songdo_arima/sarimax_training.py
+++ songdo_arima/src/songdo_arima/sarimax_training.py
@@ -1,17 +1,12 @@
 import os
+import pickle
 
 import pandas as pd
 from metr.components import TrafficData
+from pmdarima import auto_arima
+from sklearn.metrics import mean_absolute_error
 
 from songdo_arima.utils import HyperParams
-
-import os
-import itertools
-import warnings
-import pickle
-import pandas as pd
-from statsmodels.tsa.statespace.sarimax import SARIMAX, SARIMAXResults
-from sklearn.metrics import mean_absolute_error
 
 
 def train(config: HyperParams):
@@ -32,46 +27,36 @@ def train(config: HyperParams):
     train_target = train_data[target_sensor]
     valid_target = valid_data[target_sensor]
 
-    train_sensor(train_target, valid_target)
+    train_sensor(train_target, valid_target, model_output_dir)
 
-def train_sensor(train_data: pd.Series, valid_data: pd.Series):
-    p = d = q = range(0, 4)
-    P = D = Q = range(0, 2)
-    s = 24
+def train_sensor(train_data: pd.Series, valid_data: pd.Series, model_output_dir: str):
+    # auto_arima 모델 학습
+    model = auto_arima(
+        train_data,
+        start_p=0, max_p=3,
+        start_q=0, max_q=2,
+        d=None,            # 'd' 값을 자동으로 결정
+        start_P=0, max_P=1,
+        start_Q=0, max_Q=1,
+        D=None,            # 'D' 값을 자동으로 결정
+        m=24,              # 계절 주기 (시간별 데이터의 일일 계절성)
+        seasonal=True,
+        stepwise=True,     # 스텝와이즈 알고리즘 사용으로 계산 시간 단축
+        suppress_warnings=True,
+        error_action='ignore',
+        trace=True        # 진행 상황을 보려면 True로 설정
+    )
 
-    pdq = list(itertools.product(p, d, q))
-    seasonal_pdq = [(x[0], x[1], x[2], s) for x in itertools.product(P, D, Q)]
+    # 검증 데이터에 대한 예측
+    n_periods = len(valid_data)
+    forecast = model.predict(n_periods=n_periods)
+    forecast = pd.Series(forecast, index=valid_data.index)
 
-    best_mae = float('inf')
-    best_order = None
-    best_seasonal_order = None
-    best_model = None
+    # MAE 계산
+    mae = mean_absolute_error(valid_data, forecast)
+    print(f'Auto ARIMA 모델 MAE: {mae}')
 
-    # warnings.filterwarnings("ignore")  # Ignore convergence warnings
-
-    for order in pdq:
-        for seasonal_order in seasonal_pdq:
-            print(f"SARIMAX{order}x{seasonal_order} - Fitting...")
-            try:
-                model = SARIMAX(
-                    train_data,
-                    order=order,
-                    seasonal_order=seasonal_order,
-                    enforce_stationarity=False,
-                    enforce_invertibility=False,
-                )
-                model_fit: SARIMAXResults = model.fit()
-                forecast = model_fit.forecast(steps=len(valid_data))
-                mae = mean_absolute_error(valid_data, forecast)
-                if mae < best_mae:
-                    best_mae = mae
-                    best_order = order
-                    best_seasonal_order = seasonal_order
-                    best_model = model_fit
-            except Exception as e:
-                print(f"Error fitting SARIMAX{order}x{seasonal_order}: {e}")
-                continue
-
-    print(f'Best SARIMAX{best_order}x{best_seasonal_order} - MAE: {best_mae}')
-
-    return best_model
+    # 모델 저장
+    model_output_path = os.path.join(model_output_dir, "best_model.pkl")
+    with open(model_output_path, 'wb') as pkl:
+        pickle.dump(model, pkl)
